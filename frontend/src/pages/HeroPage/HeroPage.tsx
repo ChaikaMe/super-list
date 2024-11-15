0557f96f89da32858278e17fdfe9778de91425a2
ClownTripleSrank <145134985+ChaikaMe@users.noreply.github.com>
2024-11-15 14:13:02 +0200

    Quick import fix

--- frontend/src/pages/HeroPage/HeroPage.tsx
+++ frontend/src/pages/HeroPage/HeroPage.tsx
@@ -1,4 +1,4 @@
-import { useLocation, useNavigate } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import css from "./HeroPage.module.css";
 import { useEffect, useState } from "react";
 import { heroData } from "../../types/data";
